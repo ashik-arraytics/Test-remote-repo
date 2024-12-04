4e2fb05f1a036c9a3d4dfb8e0562f6afbdda95f5
Ashikur Rahman <ashikur@aethonic.com>
2024-12-04 16:47:09 +0600

    test commit @4.47 pm

---
+++ index.js
@@ -0,0 +1,9 @@
+// generating a random number
+const a = Math.random() * (10-1) + 1
+console.log(`Random value between 1 and 10 is ${a}`);
+/*
+Hello This is Ashikur Rahman ashik I a, from bogura and My village is Barisal 
+
+
+
+*/
